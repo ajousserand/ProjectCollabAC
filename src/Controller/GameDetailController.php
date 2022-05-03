4d4dfbf1963effe403349822bc119f32b19c223b
ajousserand <93247419+ajousserand@users.noreply.github.com>
2022-05-03 16:54:57 +0200

    add game detail

---
+++ src/Controller/GameDetailController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\GameRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GameDetailController extends AbstractController
+{
+    public function __construct(private GameRepository $gameRepository)
+    {
+        
+    }
+    #[Route('/jeux/{slug}', name: 'app_game_detail')]
+    public function index(string $slug): Response
+    {
+
+        $game = $this->gameRepository->findOneBy($slug);
+        return $this->render('game_detail/index.html.twig', [
+            'gameDetail' => $game,
+        ]);
+    }
+}
