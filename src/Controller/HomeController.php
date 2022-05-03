303b8be3bed2b398e303beecf93f95e9800ff7cb
ajousserand <93247419+ajousserand@users.noreply.github.com>
2022-05-03 14:36:45 +0200

    add gamme created

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(private GameRepository $gameRepository, private CommentRepository $commentRepository){
+
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-//        return $this->redirectToRoute('app_home');
+
+        $order = $this->gameRepository->findBy('createdAt','DESC',9);
 
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'LastPublishedGames' => $order,
         ]);
     }
 
