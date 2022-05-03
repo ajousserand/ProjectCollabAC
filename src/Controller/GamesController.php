09015c07439e4464cfae221c2e4a688f3df4776f
ClaireK01 <ckalsto01@gmail.com>
2022-05-03 12:45:31 +0200

    modify card view

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GamesController extends AbstractController
 {
+    public function __construct(private GameRepository $gameRepository)
+    {
+    }
+
     #[Route('/jeux', name: 'st_games')]
     public function index(): Response
     {
+        $gameEntities = $this->gameRepository->findAll();
+        dd($gameEntities);
+
         return $this->render('games/index.html.twig', [
             'controller_name' => 'GamesController',
+            'gameEntities' => $gameEntities
         ]);
     }
 }
