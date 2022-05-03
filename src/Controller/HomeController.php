7183401c0ef486d58b5ec97e312d633e114da349
ClaireK01 <ckalsto01@gmail.com>
2022-05-03 14:54:53 +0200

    update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         $gameByPublished = $this->gameRepository->findBy('publishedAt', 'DESC', 9);
-        $lastComment = $this->commentRepository->findBy('createdAt','DESC',4);
+        $lastComment = $this->commentRepository->findBy('createdAt', 'DESC', 4);
 
         return $this->render('home/index.html.twig', [
             'lastPublishedGames' => $gameByPublished,
