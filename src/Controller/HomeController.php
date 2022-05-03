bb9f61a06cfe13a632d0fecc2bf30dc93ddd0270
ClaireK01 <ckalsto01@gmail.com>
2022-05-03 15:56:43 +0200

    update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'lastPublishedGames' => $gameByPublished,
             'lastComments' => $lastComment,
-            'mostGamePlayed'=> $mostGamePlayed
+            'mostGamePlayed' => $mostGamePlayed
         ]);
     }
 }
