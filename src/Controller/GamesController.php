49ae166b28fd3c2fe3ae51ea293a436b11be2ba3
ajousserand <93247419+ajousserand@users.noreply.github.com>
2022-05-05 14:53:36 +0200

    add view langue

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -17,6 +17,7 @@ class GamesController extends AbstractController
     public function index(string $slug = ""): Response
     {
         $game = $this->gameRepository->getGameBySlug($slug);
+        // $relatedGame = $this->gameRepository->getRelatedGames();
         if ($game == null) {
             $gameEntities = $this->gameRepository->findBy([], ['publishedAt' => 'DESC']);
             return $this->render('games/index.html.twig', [
@@ -29,18 +30,24 @@ class GamesController extends AbstractController
     }
 
     #[Route('/jeux/genre/{slug}', name: 'st_games_genre')]
-    public function indexGenre(string $slug = ""): Response
+    public function indexGenre(string $slug ): Response
     {
         $gamePerGenre = $this->gameRepository->getGenreBySlug($slug);
-        // if ($game == null) {
-        //     $gameEntities = $this->gameRepository->findBy([], ['publishedAt' => 'DESC']);
-        //     return $this->render('games/index.html.twig', [
-        //         'gameEntities' => $gameEntities
-        //     ]);
-        // }
         return $this->render('genre/index.html.twig', [
             'gameDetail' => $gamePerGenre,
             'slug' => $slug
         ]);
     }
+
+    #[Route('/jeux/langue/{slug}', name: 'st_games_langue')]
+    public function indexLangue(string $slug ): Response
+    {
+        $gamePerLangue = $this->gameRepository->getLangueBySlug($slug);
+        return $this->render('langue/index.html.twig', [
+            'gameDetail' => $gamePerLangue,
+            'slug' => $slug
+        ]);
+    }
+
+
 }
