7a3c88601b9a0d587ff733f641f0ca38509d8f17
ClaireK01 <ckalsto01@gmail.com>
2022-05-06 12:23:50 +0200

    add crud user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
+use App\Form\UserType;
 use App\Repository\AccountRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +20,48 @@ class UserController extends AbstractController
     {
     }
 
+    #[Route('/index', path: 'app_user')]
+    public function index()
+    {
+        return $this->render('user/index.html.twig', [
+            'users' => $this->accountRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/create', name: 'app_form_create')]
+    public function create(Request $request, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(UserType::class, new Account());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $user->setCreatedAt(new DateTime('now'));
+            $em->persist($user);
+            $em->flush();
+            // return $this->redirectToRoute('app_user');
+        }
+
+        return $this->render('form/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/edit/{name}', name: 'app_form_edit')]
+    public function edit(Account $account, Request $request, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(UserType::class, $account);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            // return $this->redirectToRoute('app_user');
+        }
+
+        return $this->render('form/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 
     #[Route('/{name}', name: 'st_user_show')]
     public function show(string $name): Response
