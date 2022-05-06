e17206a0349b3648d7d76d161c2594f14f309ccc
ajousserand <93247419+ajousserand@users.noreply.github.com>
2022-05-06 11:30:06 +0200

    add form style

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Account;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,37 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('nickname')
-            ->add('slug')
-            ->add('createdAt');
+            ->add('name', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Nom',
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Email',
+                ]
+            ])
+            ->add('nickname', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'pseudo',
+                ]
+            ])
+            ->add('slug', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'slug',
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Soumettre',
+                'attr' => [
+                    'class' => 'btn btn-warning'
+                ]
+                ])
+            ;       
     }
 
     public function configureOptions(OptionsResolver $resolver): void
